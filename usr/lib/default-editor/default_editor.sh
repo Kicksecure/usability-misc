141063f56e6dac55a63ba6ea60bbad80e7cd4fdb
Patrick Schleizer <adrelanos@riseup.net>
2019-06-26 07:12:42 -0400

    comment

--- usr/lib/default-editor/default_editor.sh
+++ usr/lib/default-editor/default_editor.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-## Sets mousepad as the default editor for sudoedit.
+## Sets mousepad as the default editor for sudoedit if environment variable
+## SUDO_EDITOR is unset and if mousepad is installed.
 
 if [ "$SUDO_EDITOR" = "" ]; then
    if command -v mousepad 1>/dev/null 2>/dev/null  ; then
