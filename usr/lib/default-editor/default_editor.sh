c4a69675378c6096534b0892a1a58a09bb94462b
Patrick Schleizer <adrelanos@riseup.net>
2019-09-16 13:26:08 +0000

    No need to set SUDO_EDITOR. Better set VISUAL.
    sudoedit will honor VISUAL.
    
    Sets mousepad as the default editor for VISUAL if environment variable VISUAL is unset and if mousepad is installed.
    
    https://forums.whonix.org/t/use-sudoedit-in-whonix-documentation/7599

--- usr/lib/default-editor/default_editor.sh
+++ usr/lib/default-editor/default_editor.sh
@@ -3,15 +3,15 @@
 ## Copyright (C) 2019 - 2019 ENCRYPTED SUPPORT LP <adrelanos@riseup.net>
 ## See the file COPYING for copying conditions.
 
-## Sets mousepad as the default editor for sudoedit if environment variable
-## SUDO_EDITOR is unset and if mousepad is installed.
+## Sets mousepad as the default editor for VISUAL if environment variable
+## VISUAL is unset and if mousepad is installed.
 
 ## please provide /usr/bin/visual
 ## https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=758228
 
-if [ "$SUDO_EDITOR" = "" ]; then
+if [ "$VISUAL" = "" ]; then
    if command -v mousepad 1>/dev/null 2>/dev/null  ; then
-      SUDO_EDITOR="mousepad"
-      export SUDO_EDITOR
+      VISUAL="mousepad"
+      export VISUAL
    fi
 fi
