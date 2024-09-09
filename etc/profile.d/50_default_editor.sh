e6ff0d2f5cb160104bc4c55fa7b5d2c45362801f
Patrick Schleizer <adrelanos@whonix.org>
2024-09-09 14:20:22 -0400

    set `thunar` as default file manager for `xdg-mime`
    https://forums.whonix.org/t/is-catfish-the-new-default-in-the-qubes-domain-open-file-manager-quick-widget-for-whonix/20233

--- etc/profile.d/50_default_editor.sh
+++ etc/profile.d/50_default_editor.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 
-## Copyright (C) 2019 - 2023 ENCRYPTED SUPPORT LP <adrelanos@whonix.org>
+## Copyright (C) 2019 - 2024 ENCRYPTED SUPPORT LP <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
+
+#### meta start
+#### project Kicksecure
+#### category apps
+#### description
+## Source <code>/usr/libexec/default-editor/default_editor.sh</code>, if it is
+## executable.
+##
+## Add <code>/usr/share/usability-misc/xdg-override/</code> to
+## <code>XDG_CONFIG_DIRS</code> environment variable.
+#### meta end
 
 if [ -x /usr/libexec/default-editor/default_editor.sh ]; then
    . /usr/libexec/default-editor/default_editor.sh
+fi
+
+if [ -z "$XDG_DATA_DIRS" ]; then
+   XDG_DATA_DIRS=/usr/local/share/:/usr/share/
+fi
+if ! echo "$XDG_DATA_DIRS" | grep --quiet /usr/share/usability-misc/xdg-override/ ; then
+   export XDG_DATA_DIRS=/usr/share/usability-misc/xdg-override/:$XDG_DATA_DIRS
 fi
